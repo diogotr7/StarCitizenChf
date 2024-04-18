9b5ec9228dd407ad6b075cf53a1b99d72df34fef
Diogo Trindade <diogotr7@gmail.com>
2024-04-18 22:30:12 +0100

    initial commit

---
+++ CharacterDownloader/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Http.Json;
+using System.Text.Json;
+
+using var httpClient = new HttpClient();
+int page = 1;
+Directory.CreateDirectory("data");
+
+while (true)
+{
+    var response = await httpClient.GetFromJsonAsync<RootObject>($"https://www.star-citizen-characters.com/api/heads?page={page}&orderBy=latest");
+    await File.WriteAllTextAsync($"data/page{page}.json", JsonSerializer.Serialize(response, new JsonSerializerOptions { WriteIndented = true }));
+
+    page++;
+    if (response!.body.hasNextPage == false)
+        break;
+}
+
+var urls = Directory.GetFiles("data", "*.json")
+    .SelectMany(page => JsonSerializer.Deserialize<RootObject>(File.ReadAllText(page))!.body.rows.Select(row => row.dnaUrl));
+
+await File.WriteAllTextAsync("urls.txt", string.Join(Environment.NewLine, urls));
+
+public record RootObject(
+    Body body,
+    string path,
+    string query,
+    object[] cookies
+);
+
+public record Body(
+    bool hasPrevPage,
+    bool hasNextPage,
+    Rows[] rows
+);
+
+public record Rows(
+    string id,
+    string createdAt,
+    string title,
+    object[] tags,
+    User user,
+    string previewUrl,
+    string dnaUrl,
+    _count _count
+);
+
+public record User(
+    string id,
+    string name,
+    string image,
+    string starCitizenHandle
+);
+
+public record _count(
+    int characterDownloads,
+    int characterLikes
+);
+
---
+++ StarCitizenChf/Program.cs
@@ -0,0 +1,101 @@
+﻿using ZstdSharp;
+const int size = 4096;//all files are 4096 bytes long
+
+var buffers = Directory.GetFiles("data", "*.chf", SearchOption.AllDirectories)
+    .Select(File.ReadAllBytes).ToArray();
+var names = Directory.GetFiles("data", "*.chf", SearchOption.AllDirectories)
+    .Select(Path.GetFileName).ToArray();
+
+// var onesWithDeadbeef = buffers.Where(b =>
+// {
+//     var span = b.AsSpan();
+//     ReadOnlySpan<byte> deadbeef = [0xEF, 0xBE, 0xAD, 0xDE];
+//     
+//     return span.IndexOf(deadbeef) != -1;
+// }).ToArray();
+// var onesWithoutDeadbeef = buffers.Except(onesWithDeadbeef).ToArray();
+//
+// var commonBytesDeadBeef = new List<int>(size);
+// var commonBytesNoDeadBeef = new List<int>(size);
+//
+// for (var i = 0; i < size; i++)
+// {
+//     if (onesWithDeadbeef.All(b => b[i] == buffers[0][i]))
+//         commonBytesDeadBeef.Add(i);
+//     
+//     if (onesWithoutDeadbeef.All(b => b[i] == buffers[0][i]))
+//         commonBytesNoDeadBeef.Add(i);
+// }
+//
+// var valuesAtFFB = buffers.Select(i => i[0xffb]).ToArray();
+// for(var i = 0; i < buffers.Length; i++){
+//     if(valuesAtFFB[i] == 61)
+//         Console.WriteLine($"Special file: {names[i]}");
+// }
+// var ddbfvaluesAtBytes = commonBytesDeadBeef.Select(i => onesWithDeadbeef[0][i]).ToArray();
+// var ddbfasString = string.Join(", ", commonBytesDeadBeef.Select(i => $"0x{i:X2}"));
+// var ddbfvaluesAtByteHex = string.Join(", ", ddbfvaluesAtBytes.Select(b => $"0x{b:X2}"));
+//
+//
+// var nddbfvaluesAtBytes = commonBytesNoDeadBeef.Select(i => onesWithoutDeadbeef[0][i]).ToArray();
+// var nddbfasString = string.Join(", ", commonBytesNoDeadBeef.Select(i => $"0x{i:X2}"));
+// var nddbfvaluesAtByteHex = string.Join(", ", nddbfvaluesAtBytes.Select(b => $"0x{b:X2}"));
+Directory.CreateDirectory("decompressed");
+foreach (var (buffer, name) in buffers.Zip(names))
+{
+    try
+    {
+        var useful = buffer[16..^8];
+        var lastDataByte = FindLastDataByte(useful);
+        if (lastDataByte == -1)
+            continue;
+        
+        var cropped = useful[..(lastDataByte + 1)];
+        using var zstd = new Decompressor();
+        var decompressed = zstd.Unwrap(cropped.ToArray());
+        
+        Console.WriteLine($"Decompressed length: {decompressed.Length}");
+        File.WriteAllBytes(Path.Combine("decompressed", name), decompressed.ToArray());
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+return;
+
+static int FindLastDataByte(ReadOnlySpan<byte> useful)
+{
+    const int expectedLength = 4096 - 8 - 16;
+    if (useful.Length != expectedLength) throw new ArgumentException("Span must be expectedLength bytes long", nameof(useful));
+    //check for zstd magic number
+    if (useful[0] != 0x28 || useful[1] != 0xB5 || useful[2] != 0x2F || useful[3] != 0xFD)
+        throw new ArgumentException("Span must start with Zstd magic number", nameof(useful));
+
+    var containsDeadbeef = useful.IndexOf<byte>([0xEF, 0xBE, 0xAD, 0xDE]) != -1;
+
+    if (!containsDeadbeef)
+        return useful.LastIndexOfAnyExcept<byte>(0x00);
+
+    var firstDeadbeef = FindFirstDeadBeefByte(useful);
+    var slice = useful[..firstDeadbeef];
+    return slice.LastIndexOfAnyExcept<byte>(0x00);
+}
+
+static int FindFirstDeadBeefByte(ReadOnlySpan<byte> useful)
+{
+    for (var i = useful.Length - 1; i >= 0; i-=4)
+    {
+        if (useful[i] != 0xDE) 
+            return i;
+        if (useful[i - 1] != 0xAD) 
+            return i - 1;
+        if (useful[i - 2] != 0xBE) 
+            return i - 2;
+        if (useful[i - 3] != 0xEF) 
+            return i - 3;
+    }
+
+    return -1;
+}
